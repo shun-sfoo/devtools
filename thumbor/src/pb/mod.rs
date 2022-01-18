bd494b1a52686742a4cff04d0333a78b44b475f5
shun-sfoo <ganymede0915@gmail.com>
2022-01-18 17:35:16 +0800

    half thumor

---
+++ thumbor/src/pb/mod.rs
@@ -0,0 +1,53 @@
+mod abi;
+
+pub use abi::*;
+use base64::{decode_config, encode_config, URL_SAFE_NO_PAD};
+use photon_rs::transform::SamplingFilter;
+use prost::Message;
+
+impl ImageSpec {
+    pub fn new(specs: Vec<Spec>) -> Self {
+        Self { specs }
+    }
+}
+
+impl From<&ImageSpec> for String {
+    fn from(image_spec: &ImageSpec) -> Self {
+        let data = image_spec.encode_to_vec();
+        encode_config(data, URL_SAFE_NO_PAD)
+    }
+}
+
+impl TryFrom<&str> for ImageSpec {
+    type Error = anyhow::Error;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        let data = decode_config(value, URL_SAFE_NO_PAD)?;
+        Ok(ImageSpec::decode(&data[..])?)
+    }
+}
+
+impl filter::Filter {
+    pub fn to_str(&self) -> Option<&'static str> {
+        match self {
+            filter::Filter::Unspecified => None,
+            filter::Filter::Oceanic => Some("oceanic"),
+            filter::Filter::Islands => Some("islands"),
+            filter::Filter::Marine => Some("marine"),
+        }
+    }
+}
+
+// 在我们定义的 SampleFilter 和 photon_rs 的 SamplingFilter 间转换
+impl From<resize::SampleFilter> for SamplingFilter {
+    fn from(v: resize::SampleFilter) -> Self {
+        match v {
+            resize::SampleFilter::Undefined => SamplingFilter::Nearest,
+            resize::SampleFilter::Nearest => SamplingFilter::Nearest,
+            resize::SampleFilter::Triangle => SamplingFilter::Triangle,
+            resize::SampleFilter::CatmullRom => SamplingFilter::CatmullRom,
+            resize::SampleFilter::Gaussian => SamplingFilter::Gaussian,
+            resize::SampleFilter::Lanczos3 => SamplingFilter::Lanczos3,
+        }
+    }
+}
