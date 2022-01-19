a274c890c8076658c140eb3b3f7180b569a722b3
shun-sfoo <ganymede0915@gmail.com>
2022-01-19 17:26:35 +0800

    half querey

---
+++ queryer/src/convert.rs
@@ -0,0 +1,25 @@
+use sqlparser::ast::{Expr, Statement};
+
+pub struct Sql<'a> {
+    pub(crate) selection: Vec<Expr>,
+    pub(crate) condition: Option<Expr>,
+    pub(crate) source: &'a str,
+    pub(crate) order_by: Vec<(String, bool)>,
+    pub(crate) offset: Option<i64>,
+    pub(crate) limit: Option<usize>,
+}
+
+impl<'a> TryFrom<&'a Statement> for Sql<'a> {
+    type Error = anyhow::Error;
+
+    fn try_from(value: &'a Statement) -> Result<Self, Self::Error> {
+        match value {
+            Statement::Query(q) => {
+                todo!()
+            }
+            _ => {
+                todo!()
+            }
+        }
+    }
+}
