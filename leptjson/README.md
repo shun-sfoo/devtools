12bf4ce824a12d59c4a9d4fa6a2209652ccd49fe
shun-sfoo <ganymede0915@gmail.com>
2022-04-27 11:25:57 +0800

    lept parse number

--- leptjson/README.md
+++ leptjson/README.md
@@ -1,5 +1,25 @@
 # leptjson
 
+## build
+
 1. `cmake -S . -B build`
 
 2. `cmake --build build`
+
+## const
+
+### constant pointers
+
+`int* const ptr`
+
+A constant pointer is a pointer that cannot change the address its holding.
+
+### pointers to constant
+
+`const int* ptr`
+
+These type of pointes can change the address they point to but cannot change the value kept at those address.
+
+### constant pointers to constant
+
+`const int* const ptr` include above;
--- leptjson/leptjson.c
+++ leptjson/leptjson.c
@@ -1,4 +1,6 @@
 #include <assert.h>
+#include <errno.h> /* errno, ERANGE */
+#include <math.h>  /* HUGE_VAL */
 #include <stdlib.h>
 #include "leptjson.h"
 
@@ -7,6 +9,9 @@
     assert(*c->json == (ch)); \
     c->json++;                \
   } while (0)
+
+#define ISDIGIT(ch) ((ch) >= '0' && (ch) <= '9')
+#define ISDIGIT1TO9(ch) ((ch) >= '1' && (ch) <= '9')
 
 static void lept_parse_whitespace(lept_context* c) {
   const char* p = c->json;
@@ -28,17 +33,58 @@ static int lept_parse_literal(lept_context* c,
   return LEPT_PARSE_OK;
 }
 
+/* number = [ "-" ] int [ frac ] [ exp ] */
+/* int = "0" / digit1-9 *digit */
+/* frac = "." 1*digit */
+/* exp = ("e" / "E") ["-" / "+"] 1*digit */
+static int lept_parse_number(lept_context* c, lept_value* v) {
+  const char* p = c->json;
+  if (*p == '-') p++;
+
+  if (*p == '0') p++;
+  else {
+    if (!ISDIGIT1TO9(*p)) return LEPT_PARSE_INVALID_VALUE;
+    for (p++; ISDIGIT(*p); p++)
+      ;
+  }
+
+  if (*p == '.') {
+    p++;
+    if (!ISDIGIT(*p)) return LEPT_PARSE_INVALID_VALUE;
+
+    for (p++; ISDIGIT(*p); p++)
+      ;
+  }
+
+  if (*p == 'e' || *p == 'E') {
+    p++;
+
+    if (*p == '-' || *p == '+') p++;
+
+    if (!ISDIGIT(*p)) return LEPT_PARSE_INVALID_VALUE;
+    for (p++; ISDIGIT(*p); p++)
+      ;
+  }
+
+  errno = 0;
+  v->n = strtod(c->json, NULL);
+  if (errno == ERANGE && (v->n == HUGE_VAL || v->n == -HUGE_VAL))
+    return LEPT_PARSE_NUMBER_TOO_BIG;
+  c->json = p;
+  v->type = LEPT_NUMBER;
+
+  return LEPT_PARSE_OK;
+}
+
 static int lept_parse_value(lept_context* c, lept_value* v) {
   switch (*c->json) {
     case 'n': return lept_parse_literal(c, v, "null", LEPT_NULL);
-    case '\0': return LEPT_PARSE_EXPECT_VALUE;
     case 't': return lept_parse_literal(c, v, "true", LEPT_TRUE);
     case 'f': return lept_parse_literal(c, v, "false", LEPT_FALSE);
-    default: return LEPT_PARSE_INVALID_VALUE;
+    case '\0': return LEPT_PARSE_EXPECT_VALUE;
+    default: return lept_parse_number(c, v);
   }
 }
-
-static int lept_parse_number(lept_context* c, lept_value* v) {}
 
 lept_type lept_get_type(const lept_value* v) {
   assert(v != NULL);
@@ -59,7 +105,10 @@ int lept_parse(lept_value* v, const char* json) {
   lept_parse_whitespace(&c);
   if ((ret = lept_parse_value(&c, v)) == LEPT_PARSE_OK) {
     lept_parse_whitespace(&c);
-    if (*c.json != '\0') ret = LEPT_PARSE_ROOT_NOT_SINGULAR;
+    if (*c.json != '\0') {
+      v->type = LEPT_NULL;
+      ret = LEPT_PARSE_ROOT_NOT_SINGULAR;
+    }
   }
   return ret;
 }
